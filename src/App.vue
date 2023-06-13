fdbfc732e9e35102ca818adb771c8bc0ae40f617
LiorMish <liormishutin24@gmail.com>
2023-06-13 16:55:44 +0300

    logout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
-      <router-link :to="{ name: 'search' }">Search</router-link>|
-      {{ !$root.store.username }}
-      <span v-if="!$root.store.username">
-        Guest:
-        <router-link :to="{ name: 'register' }">Register</router-link>|
-        <router-link :to="{ name: 'login' }">Login</router-link>|
+      <router-link :to="{ name: 'main' }">Home</router-link>
+      <span class="link-space"></span>
+      <router-link :to="{ name: 'search' }">Search</router-link>
+      <span class="link-space2"></span>
+      <span v-if="!$root.store.username" class="guest-text">
+        Hello Guest!
+        <span class="link-space2"></span>
+        <router-link :to="{ name: 'register' }">Register</router-link>
+        <span class="link-space"></span>
+        <router-link :to="{ name: 'login' }">Login</router-link>
       </span>
-      <span v-else>
-        {{ $root.store.username }}: <button @click="Logout">Logout</button>|
+      <span v-else class="username-text">
+        {{ "Hello " + $root.store.username + "!"}}
+        <span class="link-space2"></span>
+        <button @click="Logout">Logout</button>
       </span>
     </div>
     <router-view />
@@ -21,10 +26,12 @@
 export default {
   name: "App",
   methods: {
-    Logout() {
+    async Logout() {
       this.$root.store.logout();
       this.$root.toast("Logout", "User logged out successfully", "success");
-
+      const response = await this.axios.post(
+          this.$root.store.server_domain +"/Logout"
+        );
       this.$router.push("/").catch(() => {
         this.$forceUpdate();
       });
@@ -41,19 +48,38 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  min-height: 100vh;
+  min-height: 50vh;
 }
 
 #nav {
-  padding: 30px;
+  background-color: black;
+  padding: 2vw;
+  color: white;
+  letter-spacing: 0.5vw;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  font-size: 2vw;
+  color: white;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.guest-text,
+.username-text {
+  font-size: 2vw;
+  font-family: inherit;
+}
+
+.link-space {
+  margin-right: 2vw; /* Adjust the spacing as needed */
+}
+
+.link-space2 {
+  margin-right: 16vw; /* Adjust the spacing as needed */
+}
+
 </style>
--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Four Oh Four you didn't</h1>
+    <h1>Elad want's rainbow cake</h1>
     <router-link to="/" exact>ET Go Home</router-link>
   </div>
 </template>
