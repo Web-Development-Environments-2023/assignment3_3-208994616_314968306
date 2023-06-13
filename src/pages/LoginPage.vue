964e7ed4364f03bbbc76e9fc67202ddf4c96ad26
LiorMish <liormishutin24@gmail.com>
2023-06-13 16:54:41 +0300

    logout

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -93,10 +93,10 @@ export default {
     },
     async Login() {
       try {
-        
         const response = await this.axios.post(
           // "https://test-for-3-2.herokuapp.com/user/Login",
           this.$root.store.server_domain +"/Login",
+          // "http://localhost:3000/Login",
           // "http://132.72.65.211:80/Login",
           // "http://132.73.84.100:80/Login",
 
