92e83a1393d2a2bb0e237472d37358f457db0ea6
LiorMish <liormishutin24@gmail.com>
2023-06-16 13:52:29 +0300

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,75 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'main' }">Home</router-link>
-      <span class="link-space"></span>
-      <router-link :to="{ name: 'about' }">About Us</router-link>
-      <span class="link-space"></span>
-      <router-link :to="{ name: 'search' }">Search</router-link>
-      <span class="link-space2"></span>
-      <span v-if="!$root.store.username" class="guest-text">
-        Hello Guest!
-        <span class="link-space2"></span>
-        <router-link :to="{ name: 'register' }">Register</router-link>
-        <span class="link-space"></span>
-        <router-link :to="{ name: 'login' }">Login</router-link>
-      </span>
-      <span v-else class="username-text">
-        {{ "Welcome " + $root.store.username + "!"}}
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <span class="link-space"></span>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link :to="{ name: 'main' }">Home</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link :to="{ name: 'search' }">Search</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link :to="{ name: 'about' }">About Us</router-link>
+          </b-nav-item>
+          <b-nav-item-dropdown text="Personal" right v-if="$root.store.username">
+            <b-dropdown-group>
+              <b-dropdown-item>
+                <router-link :to="{ name: 'favorites' }">Favorites</router-link>
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <router-link :to="{ name: 'personal' }">My Recipes</router-link>
+              </b-dropdown-item>
+              <b-dropdown-item>
+                <router-link :to="{ name: 'family' }">Family Recipes</router-link>
+              </b-dropdown-item>
+            </b-dropdown-group>
+          </b-nav-item-dropdown>
+          <b-nav-item>
+            <router-link :to="{ name: 'addRecipe' }">Create new Recipe</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
 
-        <span class="dropdown">
-          <select @change="handleDropdownChange">Personal
-            <option value="" disabled selected>Personal</option>
-            <option value="favorites">
-              <router-link :to="{ name: 'favorites' }">Favorites</router-link>
-            </option>
-            <option value="myRecipes">
-              <router-link :to="{ name: 'myRecipes' }">My Recipes</router-link>
-            </option>
-            <option value="family">
-              <router-link :to="{ name: 'family' }">Family Area</router-link>
-            </option>
-          </select>
-        </span>
+        <b-navbar-nav class="ml-auto" v-if="!$root.store.username">
+          <b-nav-item>
+            Hello guest:
+          </b-nav-item>
+          <b-nav-item>
+            <router-link :to="{ name: 'register' }">Register</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link :to="{ name: 'login' }">Login</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto" v-else>
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>{{ $root.store.username }}</em>
+            </template>
+            <b-dropdown-item @click="Logout">Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
 
-        <span class="link-space"></span>
-
-        <router-link :to="{ name: 'newRecipe' }">New Recipe</router-link>
-
-        <span class="link-space2"></span>
-
-        <button class="logout-button" @click="Logout">Logout</button>
-      </span>
-    </div>
     <router-view />
   </div>
 </template>
+
 
 <script>
 export default {
   name: "App",
   methods: {
-    handleDropdownChange(event) {
-      const selectedOption = event.target.value;
-      if (selectedOption === "favorites") {
-        this.$router.push({ name: "favorites" });
-      } else if (selectedOption === "myRecipes") {
-        this.$router.push({ name: "myRecipes" });
-      } else if (selectedOption === "family") {
-        this.$router.push({ name: "family" });
-      }
-    },
     async Logout() {
       this.$root.store.logout();
       this.$root.toast("Logout", "User logged out successfully", "success");
       const response = await this.axios.post(
-          this.$root.store.server_domain +"/Logout"
-        );
+        this.$root.store.server_domain + "/Logout"
+      );
       this.$router.push("/").catch(() => {
         this.$forceUpdate();
       });
@@ -82,87 +85,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: black;
-  min-height: 30vh;
-  background-image: url("https://images.pexels.com/photos/164005/pexels-photo-164005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"); 
-  background-size: cover;
-  background-repeat: no-repeat;
+  color: #0b0b0b;
+  min-height: 100vh;
 }
 
-
-#nav {
-  background-color: #CD853F;
-  padding: 0.1vw;
-  color: black;
-  letter-spacing: 0.05vw;
+/* Styles for the navigation bar */
+#app .navbar-nav a.nav-link {
+  //color: #333;
+  font-size: 25px;
 }
 
-#nav a {
-  font-weight: bold;
-  font-size: 2vw;
-  color: black;
+#app .navbar-nav .dropdown-menu {
+  margin-top: 0;
 }
 
-#nav a.router-link-exact-active {
-  color: black;
+.dropdown-menu .dropdown-item {
+  white-space: normal;
+  display: block;
 }
-
-.guest-text,
-.username-text {
-  font-weight: bold;
-  font-size: 2.5vw;
-  font-family: inherit;
-  color: white;
-}
-
-.link-space {
-  margin-right: 1.5vw; /* Adjust the spacing as needed */
-}
-
-.link-space2 {
-  margin-right: 3vw; /* Adjust the spacing as needed */
-}
-
-.logout-button {
-  /* Add your desired styles for the Logout button here */
-  background-color: white;
-  font-weight: bold;
-  color: black;
-  padding: 0.02vw 0.3vw;
-  border: none;
-  border-radius: 0.2vw;
-  font-size: 2vw;
-  cursor: pointer;
-}
-
-/* Customize the dropdown style */
-.dropdown {
-  /* Add your desired styles for the dropdown here */
-  color: #42b983; /* Red text color */
-  background-color: black; /* Light gray background color */
-  padding: 0.1vw 0.1vw;
-  font-size: 2vw;
-  cursor: pointer;
-}
-
-.dropdown select {
-  /* Add your desired styles for the select element here */
-  color: #CD853F; /* Blue text color */
-  background-color: black; /* White background color */
-  font-weight: bold;
-  font-size: 1.2vw;
-  border: none;
-  outline: none;
-  font-size: 2vw;
-}
-
-.dropdown select option {
-  /* Add your desired styles for the options here */
-  color: #2c3e50; /* Green text color */
-  font-weight: bold;
-  font-size: 1.2vw;
-  background-color: #f0f0f0; /* Light gray background color */
-}
-
 
 </style>
